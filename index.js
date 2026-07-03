4b5ea0ea602049d5e7a64c43b2461ea81387d24f
ckck912ck-lang <ckck912ck@gmail.com>
2026-07-03 16:36:48 +0900

    Add /deleteselect endpoint for batch deletion of board entries

--- index.js
+++ index.js
@@ -69,6 +69,17 @@ app.post("/delete", (req, res) => {
   });
 });
 
+app.post("/deleteselect", (req, res) => {
+  console.log(req.body);
+  const { boardIdList } = req.body;
+
+  const sqlQuery = `delete from board where id in (${boardIdList})`;
+  db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
 app.post("/update", (req, res) => {
   console.log(req.body);
   const { name, title, content, id } = req.body;

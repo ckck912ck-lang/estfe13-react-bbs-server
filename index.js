7fcd3bc1994e4df43fbad958a62a4913fe9fa8e3
ckck912ck-lang <ckck912ck@gmail.com>
2026-07-03 16:20:37 +0900

    Implement /view, /delete, and /update endpoints with error handling

--- index.js
+++ index.js
@@ -35,12 +35,46 @@ app.get("/list", (req, res) => {
   });
 });
 
+app.get("/view", (req, res) => {
+  console.log(req.query.id);
+  const id = req.query.id;
+  // const sqlQuery = `SELECT * FROM board WHERE id=${req.query.id};`;
+  const sqlQuery =
+    "SELECT title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board WHERE id=?;";
+  db.query(sqlQuery, [id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
 app.post("/write", (req, res) => {
   console.log(req.body);
   const { title, name, content } = req.body;
 
-  const sqlQuery = " insert into board (title,content,writer) values (?,?,?);";
+  const sqlQuery = "insert into board (title,content,writer) values (?,?,?);";
   db.query(sqlQuery, [title, content, name], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.post("/delete", (req, res) => {
+  console.log(req.body);
+  const { id } = req.body;
+
+  const sqlQuery = "DELETE FROM board WHERE id=?";
+  db.query(sqlQuery, [id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.post("/update", (req, res) => {
+  console.log(req.body);
+  const { name, title, content, id } = req.body;
+
+  const sqlQuery = "UPDATE board SET writer=?, title=?, content=? WHERE id=?";
+  db.query(sqlQuery, [name, title, content, id], (err, result) => {
     if (err) throw err;
     res.send(result);
   });
